046def26e9703256058bc67bc0b769c193dbe83a
Ellen Park <ellenpark781@gmail.com>
2020-01-13 23:17:07 -0500

    added the current date to page

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // create a date display that shows the current date 
+
+var date = new Date ();
+console.log (date);
+document.getElementById("date").innerHTML = date;
 // show times from 9am-5pm on the first column 
 // create text box that takes user input 
 // create a button in the last column 
