e16a432a6b3fd4a919a5cb20dbca055955f39bd5
Ellen Park <ellenpark781@gmail.com>
2020-01-14 16:59:24 -0500

    created a for loop to display time

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # HW5
 Creating a scheduling application that will allow the user to save scheduled events for each hour of the day. 
-<html>
--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 
 var date = new Date ();
 console.log (date);
-document.getElementById("date").innerHTML = date;
+document.getElementById("date").innerHTML = date.toDateString();
 // show times from 9am-5pm on the first column 
+var workTime = ["9am", "10am", "11am", "12pm"]; 
+
+for (var i=0; i < workTime.length; i++){
+var timeCol = $(".time"); 
+timeCol.attr ("data-time", workTime[i]);
+$(".time")[i].append (workTime[i]);
+}
 // create text box that takes user input 
-// create a button in the last column 
+
+// create a button in the last column
+
 // when clicked, the button will trigger the input to be saved in the local storage
+
 // ensure that the save updates data 
