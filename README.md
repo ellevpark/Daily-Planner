4780d1840aeb29d10219ec74a03ddda3f4361080
Ellen Park <ellenpark781@gmail.com>
2020-01-13 22:58:12 -0500

    created an html template and pseudo coded basic steps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HW5
 Creating a scheduling application that will allow the user to save scheduled events for each hour of the day. 
+<html>
---
+++ script.js
@@ -0,0 +1,6 @@
+// create a date display that shows the current date 
+// show times from 9am-5pm on the first column 
+// create text box that takes user input 
+// create a button in the last column 
+// when clicked, the button will trigger the input to be saved in the local storage
+// ensure that the save updates data 
