0c0a9a45085471428021801bc47d1325d66106f7
Ellen Park <ellenpark781@gmail.com>
2020-01-15 14:06:40 -0500

    color coded past, present, future hours AND added a save button icon

--- script.js
+++ script.js
@@ -7,29 +7,30 @@ console.log (currentTime);
 // create variable for times 
 
 var workTime = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var militaryTime  = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+var militaryTime  = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 //create a for loop to create elements with time, content, and a save button. 
 for (var i=0; i < workTime.length; i++){
     var timeRow = $("<div>").addClass("row");
     var timeCol = $("<div>").addClass("col-sm-1 time").text(workTime[i]);
     var taskCol = $("<textarea>").addClass("col-sm-10 task");
-    var saveCol = $("<button>").addClass("col-sm-1 save");
-
+    var saveCol = $("<button>").addClass("col-sm-1 save").prepend($("<img>",{id: "saveIcon", src: "/Users/ellenpark/Downloads/icons8-save-100.png"}));
     var toDo = localStorage.getItem(workTime[i]);
     taskCol.val(toDo);
     saveCol.attr ("data-time", workTime[i]);
     timeCol.text (workTime[i]);
 
-    if (parseInt(currentTime) === parseInt(militaryTime[i])){
-        console.log ("its a match!");
-        $(".row").css("background-color", "light pink");
-    }
-    else if (parseInt(currentTime) <= parseInt(militaryTime[i])){
-        console.log ("this is the past");
-    }
-    else if (parseInt(currentTime) >= parseInt(militaryTime[i])){
+    if (parseInt(currentTime) === militaryTime[i]){
+        console.log ("its a match!"); 
+        taskCol.addClass("currentColor");
+     }
+    else if (parseInt(currentTime) < militaryTime[i]){
         console.log ("this is the future");
+        taskCol.addClass("futureColor");
+    }
+    else if (parseInt(currentTime) > militaryTime[i]){
+        console.log ("this is the past"); 
+        taskCol.addClass("pastColor");
 
     }
   
