5d714aa9a9c38e2c7cbbc0a692e6616fb71aabff
Ellen Park <ellenpark781@gmail.com>
2020-01-14 21:40:09 -0500

    created an array to store military time

--- script.js
+++ script.js
@@ -1,57 +1,48 @@
-// create a date display that shows the current date 
-​
-// var date = new Date ();
-// console.log (date);
-// document.getElementById("date").innerHTML = date.toDateString();
-// show times from 9am-5pm on the first column 
-​
+$(document).ready(function() {
+// Display the current date and time 
 var date = moment();
 $("#date").text(date.format('MMMM Do YYYY, h:mm:ss a'));
-var currentTime = date.format() //give you just the current hour (turn into number)
-​
-var workTime = ["9am", "10am", "11am", "12pm", "1pm", "2pm"];
-//second array  = [9, 10, 11] etc.
-// military time for second array and for the current Time
-​
+var currentTime = moment().format('HH');
+console.log (currentTime);
+// create variable for times 
+
+var workTime = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var militaryTime  = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+//create a for loop to create elements with time, content, and a save button. 
 for (var i=0; i < workTime.length; i++){
     var timeRow = $("<div>").addClass("row");
-​
     var timeCol = $("<div>").addClass("col-sm-1 time").text(workTime[i]);
     var taskCol = $("<textarea>").addClass("col-sm-10 task");
     var saveCol = $("<button>").addClass("col-sm-1 save");
-​
-    var toDo = localStorage.getItem(workTime[i])
+
+    var toDo = localStorage.getItem(workTime[i]);
     taskCol.val(toDo);
-​
     saveCol.attr ("data-time", workTime[i]);
     timeCol.text (workTime[i]);
-​
-    // if(time = to our current time)
-    // else < past
-    // time -> index of work time
-    // currentTime -> moment time
-​
+
+    if (parseInt(currentTime.val) === parseInt(militaryTime[i])){
+        console.log ("its a match!");
+        $(".row").style("background-color", "light pink");
+    }
+    else if (parseInt(currentTime.val) <= parseInt(militaryTime[i])){
+        console.log ("this is the past");
+        $(".row").style("background-color", "gray");
+    }
+    else if (parseInt(currentTime.val) >= parseInt(militaryTime[i])){
+        console.log ("this is the future");
+        $(".row").style("background-color", "yellow");
+    }
+  
     timeRow.append(timeCol, taskCol, saveCol);
-    $(".container").append(timeRow, "<hr>")
+    $(".container").append(timeRow, "<hr>");
 }
-// create text box that takes user input 
-​
-// create a button in the last column
-​
-// when clicked, the button will trigger the input to be saved in the local storage
-​
-// ensure that the save updates data 
-​
+// Create a save function that will store the input into the local storage
 $(document).on("click", ".save", function(event){
     //$(this) == event.target
     var toDo = $(this).siblings(".task").val();
     var time = $(this).attr("data-time")
     localStorage.setItem(time, toDo);
+});
 })
-​
-// $(".task").each(function(){
-//       $(this)
-// })
-​
-//.innerText/.textContent => .text() (most html tags)
-//.value => .val() (just for textareas/input boxes)
+
